c35ac5b024965dfb619cdc79b5b97d3f29fd3e83
Agustina Sosa <agustinasf85@gmail.com>
2019-05-03 13:40:32 -0400

    Added styling

--- starwars/src/components/Char.js
+++ starwars/src/components/Char.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import "./StarWars.css";
 
 const Char = props => {
     return (
-        <div className={`char-info ${props.char.gender}`}>
-            <h2>
-                <strong>Name:</strong> {props.char.name}
-            </h2>
-            <p>
-                <strong>Eye Color:</strong> {props.char.eye_color}
-            </p>
-            <p>
-                <strong>Hair Color:</strong> {props.char.hair_color}
-            </p>
-                
-        
+        <div className= "char-card">
+            <div className= "char-info">
+                <h2> {props.char.name} </h2>
+                <p>
+                    <strong>Gender:</strong> {props.char.gender}
+                </p>
+                <p>
+                    <strong>Hair Color:</strong> {props.char.hair_color}
+                </p>
+                <p>
+                    <strong>Eye Color:</strong> {props.char.eye_color}
+                </p>
+            </div>
         </div>
     );
 };
